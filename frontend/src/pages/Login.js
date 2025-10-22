0f2b568faea67472b89a60bb9dc571a166df603a
prakharv19 <prakkharvarshney@gmail.com>
2025-10-22 13:19:28 +0530

    Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/login', { email, password });
+            const res = await axios.post('https://smart-farming-backend-2cxi.onrender.com/api/auth/login', { email, password });
             const { token, user } = res.data;
 
             localStorage.setItem('token', token);
