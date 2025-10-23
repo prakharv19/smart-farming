e76c0f656283242031a33855b21e759f1748e071
prakharv19 <prakkharvarshney@gmail.com>
2025-10-23 18:00:28 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,7 @@
 // frontend/src/pages/Login.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import './Login.css'; // Import the CSS
 
